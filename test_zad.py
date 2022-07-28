a452e3b76be32627e2618a03d07bfe787aa63257
dov001 <109820804+dov001@users.noreply.github.com>
2022-07-28 23:51:55 +0200

    Update test_zad.py

--- test_zad.py
+++ test_zad.py
@@ -9,45 +9,45 @@ nlp = spacy.load('en_core_web_sm')
 # prva tacka
 df1 = pd.read_csv('C:\\Users\\Korisnik\\Desktop\\intens zadatak\\descriptive_attributes.csv')
 df2 = pd.read_csv('C:\\Users\\Korisnik\\Desktop\\intens zadatak\\numeric_atributes.csv', low_memory=False)
-output1 = pd.merge(df1, df2,
+df = pd.merge(df1, df2,
                    on='movieID',
                    how='inner')
 
 # druga tacka
-print('Broj redova u kojima nije popunjena vrednost kolone isGood: ', output1['isGood'].isna().sum())
+print('Broj redova u kojima nije popunjena vrednost kolone isGood: ', df['isGood'].isna().sum())
 
 # treca tacka
-output1['averageRating'] = output1['averageRating'].fillna(0)
+df['averageRating'] = df['averageRating'].fillna(0)
 
 # cetvrta tacka
-df = output1.loc[(output1['startYear'] > 2000) & (output1['averageRating'] > 3)]
-print('Broj redova u kojima je godina početka emitovanja iznad 2000 i prosečna ocena gledalaca iznad 3: ', len(df))
-print(df)
+df4 = df.loc[(output1['startYear'] > 2000) & (df['averageRating'] > 3)]
+print('Broj redova u kojima je godina početka emitovanja iznad 2000 i prosečna ocena gledalaca iznad 3: ', len(df4))
+print(df4)
 
 # peta tacka - histogram
 plt.title("Histogram")
 plt.xlabel("prosečna ocena")
 plt.ylabel("broj elemenata")
-plt.hist(output1.averageRating, bins=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+plt.hist(df.averageRating, bins=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 # sesta tacka
-new_df = output1.groupby('titleType')['averageRating'].mean()
+df6 = df.groupby('titleType')['averageRating'].mean()
 print('Prosečna vrednost ocene za svaku kategoriju videa:')
-print(new_df)
+print(df6)
 
 # sedma tacka - spacy 'similarity'
 title = nlp("The French Connection")
-for i in range(0, len(output1['originalTitle'])):
-    new_title = nlp(output1['originalTitle'].values[i])
-    output1.loc[i, 'similarity'] = title.similarity(new_title)
+for i in range(0, len(df['originalTitle'])):
+    new_title = nlp(df['originalTitle'].values[i])
+    df.loc[i, 'similarity'] = title.similarity(new_title)
 
-print(output1.sort_values(by='similarity', ascending=False).head(10))
+print(df.sort_values(by='similarity', ascending=False).head(10))
 
 # osma tacka
 tabela = pd.DataFrame()
 k = 0
-for i in range(0, len(output1['originalTitle'])):
-    new_title = nlp(output1['originalTitle'].values[i])
+for i in range(0, len(df['originalTitle'])):
+    new_title = nlp(df['originalTitle'].values[i])
     for token in new_title:
       if((token.pos_ != 'PUNCT') & (token.pos_ != 'SYM') & (token.is_digit == 0)):
         ind = 0
@@ -62,7 +62,7 @@ for i in range(0, len(output1['originalTitle'])):
           tabela.loc[k, 'broj'] = 1
           k = k + 1
 
-tabela.to_csv('nova_tabela.csv')
+tabela.to_csv('tabela_reci.csv')
 
 # deveta tacka
 r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
